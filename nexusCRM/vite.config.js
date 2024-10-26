6dc9ca4c432dc37e89b7dafcb733bac04d855401
Thanh Bui Kim <thanhbui120501@gmail.com>
2024-10-26 08:45:27 +0700

    update main  26/10/2024 8:45

--- nexusCRM/vite.config.js
+++ nexusCRM/vite.config.js
@@ -4,18 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Build vào thư mục Laravel public
-    emptyOutDir: true, 
-    rollupOptions: {
-      input: './index.html', // Đảm bảo Rollup tìm thấy index.html
-    },// Xóa nội dung cũ trước khi build
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@': '/src', // Alias để đơn giản hóa import
-    }, // Đảm bảo Vite xử lý được cả .jsx
-  },
-  base: '/nexusCRM/', // Đảm bảo asset được load đúng
+  // build: {
+  //   outDir: 'dist', // Build vào thư mục Laravel public
+  //   emptyOutDir: true, 
+  //   rollupOptions: {
+  //     input: './index.html', // Đảm bảo Rollup tìm thấy index.html
+  //   },// Xóa nội dung cũ trước khi build
+  // },
+  // resolve: {
+  //   extensions: ['.js', '.jsx'],
+  //   alias: {
+  //     '@': '/src', // Alias để đơn giản hóa import
+  //   }, // Đảm bảo Vite xử lý được cả .jsx
+  // },
+  // base: '/nexusCRM/', // Đảm bảo asset được load đúng
 })
